48db295558594424b4d0fe9ae8be4db5959d5abf
learningbots79 <93017649+learningbots79@users.noreply.github.com>
2022-02-08 04:09:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ class Script(object):
     START_MSG = """<b>Hello {} How are you🌹,
 
 I'm an advanced filter bot with many capabilities!
-Made by @RJMALLU
+Edit By @Yash_607
 
 See <i>/help</i> for commands and more details.</b>
 """
